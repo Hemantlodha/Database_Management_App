5e8910758ecfcc8892eb64cd12f1228cb8ea1048
Hemant <hemantlodha1000@gmail.com>
2023-04-16 23:59:24 +0530

    First Project on github

--- app/src/main/java/com/hemant/university_database/splashscreen.java
+++ app/src/main/java/com/hemant/university_database/splashscreen.java
@@ -30,7 +30,7 @@ public class splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(splashscreen.this,MainActivity.class);
+                Intent intent=new Intent(splashscreen.this,login.class);
                 startActivity(intent);
                 finish();
             }
