82286fe0befbadc3c5838017f94cf63352081095
Hemant <hemantlodha1000@gmail.com>
2023-05-22 17:33:34 +0530

    First Project on github

--- app/src/main/java/com/hemant/university_database/login.java
+++ app/src/main/java/com/hemant/university_database/login.java
@@ -9,15 +9,25 @@ import android.widget.Button;
 
 public class login extends AppCompatActivity {
      Button button;
+     Button button7;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         button=findViewById(R.id.button);
+        button7=findViewById(R.id.button7);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(login.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(login.this,create_acc.class);
                 startActivity(intent);
                 finish();
             }
