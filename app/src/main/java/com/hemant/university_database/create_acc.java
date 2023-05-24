87c344075c875c85b74f286a4171c5d91525441a
Hemant <hemantlodha1000@gmail.com>
2023-05-24 15:57:27 +0530

    First Project on github

--- app/src/main/java/com/hemant/university_database/create_acc.java
+++ app/src/main/java/com/hemant/university_database/create_acc.java
@@ -2,13 +2,66 @@ package com.hemant.university_database;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class create_acc extends AppCompatActivity {
-
+    Button button8;
+    TextInputLayout username,phone,gmail,password,passc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
+        button8=findViewById(R.id.button8);
+        username=findViewById(R.id.Username);
+        phone=findViewById(R.id.phone);
+        gmail=findViewById(R.id.gmail);
+        password=findViewById(R.id.password);
+        passc=findViewById(R.id.passc);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user=username.getEditText().getText().toString();
+                String pho=phone.getEditText().getText().toString();
+                String gma=gmail.getEditText().getText().toString();
+                String p1=password.getEditText().getText().toString();
+                String p2=passc.getEditText().getText().toString();
+                if(!user.isEmpty()) {
+                    if(!pho.isEmpty()) {
+                        if(!gma.isEmpty()) {
+                            if(!p1.isEmpty()) {
+                                if(!p2.isEmpty()) {
+                                    if(p1==p2){
+
+                                    }
+                                    else{
+                                        passc.setError("password does not match");
+                                    }
+                                }
+                                else{
+                                    passc.setError("Please confirm the Password");
+                                }
+                            }
+                            else{
+                                password.setError("Please enter the Password");
+                            }
+                        }
+                        else{
+                            gmail.setError("Please enter the gmail");
+                        }
+                    }
+                    else{
+                        phone.setError("Please enter the Phone number");
+                    }
+                }
+                else{
+                    username.setError("Please enter the username");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/hemant/university_database/login.java
+++ app/src/main/java/com/hemant/university_database/login.java
@@ -5,23 +5,44 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class login extends AppCompatActivity {
      Button button;
      Button button7;
+     TextInputLayout user;
+     TextInputLayout pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         button=findViewById(R.id.button);
         button7=findViewById(R.id.button7);
+        user=findViewById(R.id.user);
+        pass=findViewById(R.id.pass);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(login.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                String us=user.getEditText().getText().toString();
+                String pas=pass.getEditText().getText().toString();
+                if(!us.isEmpty()) {
+                    if(!pas.isEmpty()) {
+                        Intent intent = new Intent(login.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                    else{
+                        pass.setError("Please enter the Password");
+                    }
+                }
+                else
+                {
+                  user.setError("Please enter the username");
+                }
             }
         });
         button7.setOnClickListener(new View.OnClickListener() {
