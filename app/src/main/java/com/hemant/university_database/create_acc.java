aecb6b1faadf03f2ed7c48fa685849c5dfbdf755
Hemant <hemantlodha1000@gmail.com>
2023-06-22 19:27:34 +0530

    First Project on github

--- app/src/main/java/com/hemant/university_database/create_acc.java
+++ app/src/main/java/com/hemant/university_database/create_acc.java
@@ -1,5 +1,7 @@
 package com.hemant.university_database;
 
+import static java.util.concurrent.TimeUnit.*;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,32 +10,71 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.ActionCodeSettings;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.concurrent.TimeUnit;
+
 public class create_acc extends AppCompatActivity {
-    Button button8;
+    Button button8,button11;
     TextInputLayout username,phone,gmail,password,passc;
-    FirebaseDatabase firebaseDatabase;
-    private FirebaseAuth mAuth;
-    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_acc);
         button8=findViewById(R.id.button8);
+        button11=findViewById(R.id.button11);
         username=findViewById(R.id.Username);
         phone=findViewById(R.id.phone);
         gmail=findViewById(R.id.gmail);
         password=findViewById(R.id.password);
         passc=findViewById(R.id.passc);
+        ProgressBar progressBar=findViewById(R.id.progress);
+        ProgressBar progressBar1=findViewById(R.id.progress1);
+        button11.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button11.setVisibility(View.INVISIBLE);
+                progressBar1.setVisibility(View.VISIBLE);
+                String email=gmail.getEditText().getText().toString();
+                ActionCodeSettings actionCodeSettings =
+                        ActionCodeSettings.newBuilder()
+                                // URL you want to redirect back to. The domain (www.example.com) for this
+                                // URL must be whitelisted in the Firebase Console.
+                                .setUrl("https://www.example.com/finishSignUp?cartId=1234")
+                                // This must be true
+                                .setHandleCodeInApp(true)
+                                .setIOSBundleId("com.example.ios")
+                                .setAndroidPackageName(
+                                        "com.example.android",
+                                        true, /* installIfNotAvailable */
+                                        "12"    /* minimumVersion */)
+                                .build();
+                FirebaseAuth auth = FirebaseAuth.getInstance();
+                auth.sendSignInLinkToEmail(email, actionCodeSettings)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(create_acc.this, "Mail is sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -43,32 +84,41 @@ public class create_acc extends AppCompatActivity {
                 String p1=password.getEditText().getText().toString();
                 String p2=passc.getEditText().getText().toString();
                 if(!user.isEmpty()) {
-                    if(!pho.isEmpty()) {
+                    if(!pho.isEmpty() && pho.length()==13) {
                         if(!gma.isEmpty()) {
                             if(!p1.isEmpty()) {
                                 if(!p2.isEmpty()) {
                                     if(p1.equalsIgnoreCase(p2)){
-                                        Intent intent= new Intent(create_acc.this,otp.class);
-                                        startActivity(intent);
-                                        finish();
-                                        mAuth = FirebaseAuth.getInstance();
-                                        mAuth.createUserWithEmailAndPassword(gma, p1)
-                                                .addOnCompleteListener(create_acc.this, new OnCompleteListener<AuthResult>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<AuthResult> task) {
-                                                        if (task.isSuccessful()) {
-                                                            // Sign in success, update UI with the signed-in user's information
-//                                                            firebaseDatabase=FirebaseDatabase.getInstance();
-//                                                            reference=firebaseDatabase.getReference("Studata user");
-//                                                            storing_data str= new storing_data(user,pho,gma,p1);
-//                                                            reference.child(user).setValue(str);
-                                                            Toast.makeText(create_acc.this, "Account is created", Toast.LENGTH_SHORT).show();
-                                                            Intent intent= new Intent(create_acc.this,login.class);
-                                                            startActivity(intent);
-                                                            finish();
-                                                        }
-                                                    }
-                                                });
+                                        button8.setVisibility(View.INVISIBLE);
+                                        progressBar.setVisibility(View.VISIBLE);
+                                        PhoneAuthProvider.getInstance().verifyPhoneNumber(pho, 60, SECONDS, create_acc.this, new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                                            @Override
+                                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                                button8.setVisibility(View.VISIBLE);
+                                                progressBar.setVisibility(View.INVISIBLE);
+                                                Toast.makeText(create_acc.this, "Account was created without otp", Toast.LENGTH_SHORT).show();
+                                            }
+
+                                            @Override
+                                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                                button8.setVisibility(View.VISIBLE);
+                                                progressBar.setVisibility(View.INVISIBLE);
+                                                Toast.makeText(create_acc.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+
+                                            @Override
+                                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                                button8.setVisibility(View.VISIBLE);
+                                                progressBar.setVisibility(View.INVISIBLE);
+                                                Intent intent = new Intent(create_acc.this,otp.class);
+                                                intent.putExtra("mobile",pho);
+                                                intent.putExtra("botp",s);
+                                                intent.putExtra("gmail",gma);
+                                                intent.putExtra("password",p1);
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        });
                                     }
                                     else{
                                         passc.setError("password does not match");
--- app/src/main/java/com/hemant/university_database/otp.java
+++ app/src/main/java/com/hemant/university_database/otp.java
@@ -1,14 +1,92 @@
 package com.hemant.university_database;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class otp extends AppCompatActivity {
-
+    Button button,button9;
+    TextView textView19;
+    EditText editText1,editText2,editText3,editText4,editText5,editText6;
+    FirebaseDatabase firebaseDatabase;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otp);
+        button=findViewById(R.id.button10);
+        button9=findViewById(R.id.button9);
+        textView19=findViewById(R.id.textView19);
+        editText1=findViewById(R.id.editText1);
+        editText2=findViewById(R.id.editText2);
+        editText3=findViewById(R.id.editText3);
+        editText4=findViewById(R.id.editText4);
+        editText5=findViewById(R.id.editText5);
+        editText6=findViewById(R.id.editText6);
+        textView19.setText("mobile no.: "+getIntent().getStringExtra("mobile"));
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              String sotp=getIntent().getStringExtra("botp") ;
+              String gma=getIntent().getStringExtra("gmail");
+              String p1=getIntent().getStringExtra("password");
+              String uotp=editText1.getText().toString()+editText2.getText().toString()+editText3.getText().toString()+editText4.getText().toString()+editText5.getText().toString()+editText6.getText().toString();
+              if(uotp!=null){
+                  PhoneAuthCredential phoneAuthCredential= PhoneAuthProvider.getCredential(sotp,uotp);
+                  FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                      @Override
+                      public void onComplete(@NonNull Task<AuthResult> task) {
+                          if(task.isSuccessful()){
+                              mAuth = FirebaseAuth.getInstance();
+                              mAuth.createUserWithEmailAndPassword(gma, p1)
+                                      .addOnCompleteListener(otp.this, new OnCompleteListener<AuthResult>() {
+                                          @Override
+                                          public void onComplete(@NonNull Task<AuthResult> task) {
+                                              if (task.isSuccessful()) {
+                                                  // Sign in success, update UI with the signed-in user's information
+//                                      firebaseDatabase= FirebaseDatabase.getInstance();
+//                                      reference=firebaseDatabase.getReference("Studata user");
+//                                      storing_data str= new storing_data(user,pho,gma,p1);
+//                                      reference.child(user).setValue(str);
+                                                  Toast.makeText(otp.this, "Account is created", Toast.LENGTH_SHORT).show();
+                                                  Intent intent= new Intent(otp.this,login.class);
+                                                  startActivity(intent);
+                                                  finish();
+                                              }
+                                          }
+                                      });
+                          }
+                          else{
+                              Toast.makeText(otp.this, "Wrong otp", Toast.LENGTH_SHORT).show();
+                          }
+                      }
+                  });
+              }
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(otp.this,create_acc.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
